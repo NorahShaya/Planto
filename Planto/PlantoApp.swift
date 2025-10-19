ee550353daf03c7fe7985ca34923b14792cca392
NorahShaya <norahalshaya4@gmail.com>
2025-10-19 10:27:05 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Norah Aldawsari on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantoTests/PlantoTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlantoTests.swift
+//  PlantoTests
+//
+//  Created by Norah Aldawsari on 27/04/1447 AH.
+//
+
+import Testing
+
+struct PlantoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
